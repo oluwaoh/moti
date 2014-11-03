3cf314707dc697ae6e08cb7e8acfb9a868ad5629
Mehta, Devraj <Devraj.Mehta@gtri.gatech.edu>
2014-11-03 11:21:48 -0500

    improved focus-check messaging so never empty

--- extension/src/focus-check/focus-check.js
+++ extension/src/focus-check/focus-check.js
@@ -11,11 +11,11 @@ angular.module('focus-check', [])
 
 .controller('FocusCheckCtrl', function($window, $element, $scope) {
     var ctrl = this;
-    var msg = 'tab or click to focus on page';
+    var msg = 'press tab or click to focus on page';
     var windowEl = angular.element($window);
     $element.html(msg);
     windowEl.on('focus', function() {
-        $element.html('');
+        $element.html('click or press key to select a bookmark');
     });
     windowEl.on('blur', function() {
         $element.html(msg);
