8cf4c9a9c8e9dce52cf2b783fc4c08dcf1af8d03
Devraj Mehta <devm33@gmail.com>
2014-12-24 14:59:08 -0500

    added smarter positioning for context menu

--- extension/src/context-menu/context-menu.js
+++ extension/src/context-menu/context-menu.js
@@ -6,13 +6,14 @@ angular.module('context-menu', [])
         scope: {},
         transclude: true,
         template: '<div ng-transclude></div>',
-        controller: function($scope, $element, $window) {
+        controller: function($scope, $element, $window, $rootScope, $window) {
             function open(event) {
+                $rootScope.$emit('contextmenu');
                 event.preventDefault();
                 $element.addClass('show');
                 $element.css({
-                    top: event.pageY + 'px',
-                    left: event.pageX + 'px',
+                    top: Math.min(event.clientY, getMaxYPos()) + 'px',
+                    left: event.clientX + 'px',
                 });
             }
             function close() {
@@ -20,11 +21,16 @@ angular.module('context-menu', [])
             }
             $element.parent().on('contextmenu', open);
             angular.element($window).on('click', close);
+            $rootScope.$on('contextmenu', close);
 
             $scope.$on('$destroy', function() {
                 $element.parent().off('contextmenu', open);
                 angular.element($window).off('click', close);
             });
+
+            function getMaxYPos() {
+                return $window.innerHeight - $element[0].offsetHeight;
+            }
         }
     };
 })
@@ -32,10 +38,11 @@ angular.module('context-menu', [])
 .directive('contextMenuItem', function() {
     return {
         restrict: 'E',
+        require: '^contextMenu',
         scope: { handler: '&' },
         transclude: true,
         template: '<div ng-transclude></div>',
-        link: function(scope, element) {
+        link: function(scope, element, attrs, parentCtrl) {
             element.on('click', function(event) {
                 scope.handler();
                 event.preventDefault();
